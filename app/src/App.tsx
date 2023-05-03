6d667bed90f3275f5f079163173df5d2ac4b6948
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-05-02 22:54:44 -0300

    Configuração do app e estilizações iniciais com styled components

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as C from "./App.styles";
+
+const App = () => {
+    return (
+        <C.Container>
+            <C.Header>
+                <C.HeaderText>Sistema Financeiro</C.HeaderText>
+            </C.Header>
+            <C.Body>...</C.Body>
+        </C.Container>
+    );
+};
+
+export default App;
